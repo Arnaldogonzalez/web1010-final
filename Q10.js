05e2bccc21e67a8d00b03e3acc65155f505437de
Arnaldo Gonzalez <agonzalez@fvi.edu>
2017-02-21 21:59:13 -0500

    Complete final exam

---
+++ Q10.js
@@ -0,0 +1,7 @@
+// Question 10
+// Print to the console a sentence that uses the following variables:
+var totalPower = 32
+var generator = 1
+
+// Answer:
+console.log("Generator#" + generator + " has" +" "+ totalPower + "MW" + ".");
